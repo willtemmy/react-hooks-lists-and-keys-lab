847d11e000716f0e1ac3986cec3abdf5ea918029
William Mtemi Mponzi <willbrian009@gmail.com>
2023-04-27 12:59:18 +0300

    complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,13 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const link = links.map((lin) => {
+    return <a key={lin} href={lin}>{lin}</a>
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{/* display an <a> tag for each link here */}
+    {link}
+  </nav>;
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,10 +3,15 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{/* render ProjectItem components here */}
+      {projects.map((projects) =>(
+        <ProjectItem key={projects.id} {...projects} />
+      ))}
+      </div>
     </div>
   );
 }
